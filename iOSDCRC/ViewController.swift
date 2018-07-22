e7395a4205dcc9f18e366828037772c6ec830d39
Kazuya Ueoka <info@fromkk.info>
2018-07-22 10:46:19 +0900

    Initial Commit

---
+++ iOSDCRC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSDCRC
+//
+//  Created by Kazuya Ueoka on 2018/07/22.
+//  Copyright © 2018 Kazuya Ueoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
