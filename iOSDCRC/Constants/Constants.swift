d6db04c76498a87514c606df022fc1850caae0bf
Kazuya Ueoka <info@fromkk.info>
2018-07-27 16:42:59 +0900

    add Constants.Date

--- iOSDCRC/Constants/Constants.swift
+++ iOSDCRC/Constants/Constants.swift
@@ -15,4 +15,13 @@ extension Constants {
         static let consumerKey = "YOUR CUSTOMER KEY"
         static let consumerSecret = "YOUR CUSTOMER SECRET KEY"
     }
+    
+    class Date {
+        public static let minutes: Double = 60.0
+        public static let hour = Constants.Date.minutes * 60.0
+        public static let day = Constants.Date.hour * 24.0
+        public static let week = Constants.Date.day * 7.0
+        public static let month = Constants.Date.day * 30.0
+        public static let year = Constants.Date.day * 365.0
+    }
 }
