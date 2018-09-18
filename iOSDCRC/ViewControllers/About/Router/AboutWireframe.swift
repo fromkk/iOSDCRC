40f4e5a1001f477e40ae2d2a068f049c17c2d870
ktanaka117 <>
2018-09-18 14:31:18 +0900

    ファイルを追加

---
+++ iOSDCRC/ViewControllers/About/Router/AboutWireframe.swift
@@ -0,0 +1,9 @@
+//
+//  AboutWireframe.swift
+//  iOSDCRC
+//
+//  Created by Kenji Tanaka on 2018/09/18.
+//  Copyright © 2018年 Kazuya Ueoka. All rights reserved.
+//
+
+import Foundation
---
+++ iOSDCRCTests/AboutPresenterTests.swift
@@ -0,0 +1,9 @@
+//
+//  AboutPresenterTests.swift
+//  iOSDCRCTests
+//
+//  Created by Kenji Tanaka on 2018/09/18.
+//  Copyright © 2018年 Kazuya Ueoka. All rights reserved.
+//
+
+import Foundation
